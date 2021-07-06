ff85f13dbbfcecf7d442a388cc70c65d0c472c5e
NutanMcc <nutan@multichannelcreative.co.uk>
2021-07-06 20:14:45 +0530

    logo changes

--- resources/views/frontend/includes/header.blade.php
+++ resources/views/frontend/includes/header.blade.php
@@ -3,7 +3,7 @@
         <div class="row header-main-col">
             <div class="col-xs-6 col-sm-3 header-logo-col">
                 <span id="menuOpener" style="display: none;"><i class="fa fa-bars"></i></span>
-                <a href="#"><img src="{{asset('images/logo.png')}}" alt="" class="img-fluid"></a>
+                <a href="{{url('/')}}"><img src="{{asset('images/logo.png')}}" alt="" class="img-fluid"></a>
             </div>
             <div class="col-xs-6 col-sm-9 d-flex header-menu-col">
                 <ul class="list-inline d-flex site-menu site-main-menu">
