681c438125d7d5809b824cb0e1882a595fb14110
ramseo <rambabu.seo@gmail.com>
2021-08-05 18:03:34 +0530

    service add .

--- resources/views/backend/prices/service.blade.php
+++ resources/views/backend/prices/service.blade.php
@@ -1,24 +1,24 @@
     <div class="col-12">
         <div class="form-group">
-            @if($service->input_type == 'text')
+            @if($service->input_type_value == 'text')
                 {{ Form::label('name', Str::title($service->name) ) }}
-                {{ Form::text("service_type[$service->id]", isset($pricesData[$service->id]['service_type'])? $pricesData[$service->id]['service_type']:null, array('class' => 'form-control')) }}
+                {{ Form::text("input_type_value[$service->id]", isset($pricesData[$service->id]['service_type'])? $pricesData[$service->id]['service_type']:null, array('class' => 'form-control')) }}
             @endif
-            @if($service->input_type == 'number')
+            @if($service->input_type_value == 'number')
                 {{ Form::label('name',  Str::title($service->name) ) }}
-                {{ Form::number("service_type[$service->id]", isset($pricesData[$service->id]['service_type'])? $pricesData[$service->id]['service_type']:null, array('class' => 'form-control')) }}
+                {{ Form::number("input_type_value[$service->id]", isset($pricesData[$service->id]['service_type'])? $pricesData[$service->id]['service_type']:null, array('class' => 'form-control')) }}
             @endif
-            @if($service->input_type == 'price')
+            @if($service->input_type_value == 'price')
                 {{ Form::label('name',  Str::title($service->name) ) }}
                     <div class="input-group mb-3">
                         <div class="input-group-prepend">
                             <span class="input-group-text" id="basic-addon1">₹</span>
                         </div>
                         @php $placeholder = 'Price per Day'; @endphp
-                        @if($service->service_on_basis != 'day')
+                        @if($service->service_on_basis == 'complete')
                             @php $placeholder = 'Price  for '. $service->name  ; @endphp
                         @endif
-                        {{ Form::number("service_type[$service->id]", isset($pricesData[$service->id]['service_type'])? $pricesData[$service->id]['service_type']:null, array('class' => 'form-control','placeholder'=>$placeholder)) }}
+                        {{ Form::number("input_type_value[$service->id]", isset($pricesData[$service->id]['input_type_value'])? $pricesData[$service->id]['input_type_value']:null, array('class' => 'form-control','placeholder'=>$placeholder)) }}
                     </div>
 
             @endif
