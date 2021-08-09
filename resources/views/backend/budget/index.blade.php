2a26233e70ed95de4fc7cd6a36148d84115fa2de
NutanMcc <nutan@multichannelcreative.co.uk>
2021-08-09 18:17:13 +0530

    fsdf

--- resources/views/backend/budget/index.blade.php
+++ resources/views/backend/budget/index.blade.php
@@ -1,7 +1,7 @@
 @extends('backend.layouts.app')
 
 @section('title')
-<title>Tickets | Index</title>
+<title>Budget | Index</title>
 @endsection
 {{--@section('header-title')--}}
 {{--    Manage Tickets--}}
@@ -17,10 +17,10 @@
             <div class="row">
                 <div class="col-8">
                     <h4 class="card-title mb-0">
-                        <i class="c-icon cil-people"></i> Service <small class="text-muted">Data Table Service</small>
+                        <i class="c-icon cil-people"></i> Budget <small class="text-muted">Data Table Budget</small>
                     </h4>
                     <div class="small text-muted">
-                        {{ Str::title('service') }} Management Dashboard
+                        Budget Management Dashboard
                     </div>
                 </div>
                 <div class="col-4">
@@ -28,7 +28,7 @@
                         <a href="{{ route("backend.type.index") }}" class="btn btn-secondary btn-sm ml-1" data-toggle="tooltip" title="Type List"><i class="fas fa-list-ul"></i> List</a>
                     </div>
                     <div class="float-right">
-                        <a href='{{ route("backend.service.create").'/'. $typeId}}'
+                        <a href='{{ route("backend.budget.create").'/'. $typeId}}'
                            class='btn btn-success btn-sm'
                            data-toggle="tooltip"
                            title="{{__('Create')}}">
@@ -45,8 +45,9 @@
                         <table id="datatable" class="table table-bordered table-hover table-responsive-sm">
                             <thead>
                             <th> # </th>
-                            <th> Name </th>
-                            <th> Placeholder </th>
+                            <th> Filter </th>
+                            <th> Min Amount </th>
+                            <th> Max Amount </th>
                             <th> Action </th>
                             </thead>
                             <tbody>
@@ -88,11 +89,12 @@
         var table = $('#datatable').DataTable({
             processing: true,
             serverSide: true,
-                ajax: "{{ route('backend.service.index').'/'. $typeId}}",
+                ajax: "{{ route('backend.budget.index').'/'. $typeId}}",
                 columns: [
                     {data: 'id', name: 'id'},
-                    {data: 'name', name: 'name'},
-                    {data: 'placeholder', name: 'placeholder'},
+                    {data: 'filter', name: 'filter'},
+                    {data: 'min', name: 'min'},
+                    {data: 'max', name: 'max'},
                     {data: 'action', name: 'action', orderable: false, searchable: false},
                 ],
             columnDefs: [{
@@ -102,28 +104,6 @@
             "order": [[1, 'desc']]
             });
 
-        // function ticket_status(data){
-        //     let select = '<div class="status">';
-        //     select += '<select class="form-control ticket_status" name="status" ticket="'+data.id+'">';
-        //     let status = {open:'Open', pending:'Pending',answered:'Answered',resolved:'Resolved',closed:'Closed', spam:'Span'};
-        //        $.each(status , function (k, v){
-        //            if(data.status == k){
-        //                select += '<option value="'+k+'" selected="selected">'+v+'</option>';
-        //            }else{
-        //                select += '<option value="'+k+'">'+v+'</option>';
-        //            }
-        //        });
-        //     select += '</select>';
-        //     select += '</div>';
-        //     return select;
-        // }
-
-        {{--$(document).on('change', '.ticket_status', function (){--}}
-        {{--    let data ={ _token: '{{ csrf_token() }}', ticket_id: $(this).attr('ticket'), status: $(this).val() }--}}
-        {{--    $.post('{{ route('admin.tickets.update') }}', data, function (data){--}}
-        {{--        toastr.success("Ticket updated successfully!");--}}
-        {{--    },'json');--}}
-        {{--});--}}
     </script>
 
 @endpush
