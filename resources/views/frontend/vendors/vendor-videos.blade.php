4fd28d2750af8ecb2b59283df23bf7f6f227f69b
NutanMcc <nutan@multichannelcreative.co.uk>
2021-09-05 23:32:12 +0530

    video embed

--- resources/views/frontend/vendors/vendor-videos.blade.php
+++ resources/views/frontend/vendors/vendor-videos.blade.php
@@ -5,7 +5,7 @@
     <div class="videos-main-col">
         @foreach($vendor_videos as $vendor_video)
             <div class="videe-item">
-                <iframe src="{{$vendor_video->url}}" frameborder="0"></iframe>
+                {{$vendor_video->url}}
             </div>
         @endforeach
     </div>
