697a24bb7354792ca51ba8008ce63ba11fb3eff1
NutanMahto <nutankumarmahto@gmail.com>
2022-01-14 00:29:49 +0530

    register

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use App\Models\User;
 
 class VerifyEmailController extends Controller
 {
@@ -16,20 +17,37 @@ class VerifyEmailController extends Controller
      *
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function __invoke(EmailVerificationRequest $request)
+
+    public function __invoke(Request $request): RedirectResponse
     {
-        if (!auth()->check()) {
-            auth()->loginUsingId($request->route('id'));
+        $user = User::find($request->route('id')); //takes user ID from verification link. Even if somebody would hijack the URL, signature will be fail the request
+        if ($user->hasVerifiedEmail()) {
+            return redirect()->intended(config('fortify.home') . '?verified=1');
         }
 
-        if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(RouteServiceProvider::HOME.'?verified=1');
+        if ($user->markEmailAsVerified()) {
+            event(new Verified($user));
         }
 
-        if ($request->user()->markEmailAsVerified()) {
-            event(new Verified($request->user()));
-        }
+        $message = __('Your email has been verified.');
 
-        return redirect()->intended(RouteServiceProvider::HOME.'?verified=1');
+        return redirect('login')->with('status', $message); //if user is already logged in it will redirect to the dashboard page
     }
+
+//    public function __invoke(EmailVerificationRequest $request)
+//    {
+//        if (!auth()->check()) {
+//            auth()->loginUsingId($request->route('id'));
+//        }
+//
+//        if ($request->user()->hasVerifiedEmail()) {
+//            return redirect()->intended(RouteServiceProvider::HOME.'?verified=1');
+//        }
+//
+//        if ($request->user()->markEmailAsVerified()) {
+//            event(new Verified($request->user()));
+//        }
+//
+//        return redirect()->intended(RouteServiceProvider::HOME.'?verified=1');
+//    }
 }
