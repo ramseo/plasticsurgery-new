9c54b7efd37a6156e19a021bcb6efa95404d306e
NutanMahto <nutankumarmahto@gmail.com>
2022-01-14 00:39:13 +0530

    register

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\RedirectResponse;
 use App\Models\User;
 
 class VerifyEmailController extends Controller
@@ -17,17 +18,32 @@ class VerifyEmailController extends Controller
      *
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function __invoke(EmailVerificationRequest $request)
+
+    public function __invoke(Request $request): RedirectResponse
     {
         $user = User::find($request->route('id')); //takes user ID from verification link. Even if somebody would hijack the URL, signature will be fail the request
         if ($user->hasVerifiedEmail()) {
-            return redirect()->intended(RouteServiceProvider::HOME.'?verified=1');
+            return redirect()->intended(RouteServiceProvider::HOME . '?verified=1');
         }
 
         if ($user->markEmailAsVerified()) {
             event(new Verified($user));
         }
 
-        return redirect()->intended(RouteServiceProvider::HOME.'?verified=1');
+        return redirect()->intended(RouteServiceProvider::HOME.'?verified=1'); //if user is already logged in it will redirect to the dashboard page
     }
+
+//    public function __invoke(EmailVerificationRequest $request)
+//    {
+//        $user = User::find($request->route('id')); //takes user ID from verification link. Even if somebody would hijack the URL, signature will be fail the request
+//        if ($user->hasVerifiedEmail()) {
+//            return redirect()->intended(RouteServiceProvider::HOME.'?verified=1');
+//        }
+//
+//        if ($user->markEmailAsVerified()) {
+//            event(new Verified($user));
+//        }
+//
+//        return redirect()->intended(RouteServiceProvider::HOME.'?verified=1');
+//    }
 }
