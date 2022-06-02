dd4254ddbaa4440508490af66f89ddbddb13b3b5
NutanMahto <nutankumarmahto@gmail.com>
2022-06-02 21:51:16 +0530

    ->stateless()

--- app/Http/Controllers/Auth/SocialLoginController.php
+++ app/Http/Controllers/Auth/SocialLoginController.php
@@ -56,7 +56,7 @@ class SocialLoginController extends Controller
     public function handleProviderCallback($provider)
     {
         try {
-            $user = Socialite::driver($provider)->user();
+            $user = Socialite::driver($provider)->stateless()->user();
 
             $authUser = $this->findOrCreateUser($user, $provider);
 
