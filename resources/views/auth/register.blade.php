cb31d3f5d98cc9a1103e7eef617070822898473c
NutanMahto <nutankumarmahto@gmail.com>
2022-06-02 14:44:40 +0530

    social login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -32,13 +32,13 @@
                 @if(env('FACEBOOK_ACTIVE'))
                 <a href="{{route('social.login', 'facebook')}}" class="btn btn-block btn-outline-info">
                     <span class="btn-inner--icon"> <i class="fab fa-facebook"></i> </span>
-                    <span class="btn-inner--text">Log in with Facebook</span>
+                    <span class="btn-inner--text">Sign Up with Facebook</span>
                 </a>
                 @endif
                 @if(env('GOOGLE_ACTIVE'))
                 <a href="{{route('social.login', 'google')}}" class="btn btn-block btn-outline-primary">
                     <span class="btn-inner--icon"><i class="fab fa-google"></i> </span>
-                    <span class="btn-inner--text"> Log in with Google</span>
+                    <span class="btn-inner--text">Sign Up with Google</span>
                 </a>
                 @endif
             </div>
