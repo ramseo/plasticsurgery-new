fb2126fc942e301c4d4be3e76f73377a3e1017b3
NutanMahto <nutankumarmahto@gmail.com>
2022-07-30 20:16:03 +0530

    vendor

--- resources/views/frontend/vendors/search-vendor.blade.php
+++ resources/views/frontend/vendors/search-vendor.blade.php
@@ -3,7 +3,27 @@
 @section('title') {{app_name()}} @endsection
 
 @section('content')
+<section id="breadcrumb-section">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{url('/')}}">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Vendor Search</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>
 
-<h1>hi</h1>
+    <section id="vendor-detail-section">
+        <div class="container-fluid">
+            <div class="row vendor-detail-main-col">
+			<h1 style="color: red;">hi</h1>
+			</div>
+		</div>
+	</section>
 
 @endsection
