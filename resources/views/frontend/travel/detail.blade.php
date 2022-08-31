48a90ab26fdaacb31978267af52596215cb9ab51
suraj-ux <surajlink99@gmail.com>
2022-08-31 10:15:21 +0530

    Successfull Create detail page

--- resources/views/frontend/travel/detail.blade.php
+++ resources/views/frontend/travel/detail.blade.php
@@ -21,14 +21,140 @@
     </section>
 
     
-    <section id="`">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-xs-12 col-sm-12">
-                    <h1>Honeymoon Ideas detail</h1>
-                </div>
+    <section id="vendor-detail-section">
+   <div class="container-fluid">
+      <div class="row vendor-detail-main-col  hny">
+         <div class="col-xs-12 col-sm-7 vendor-detail-img-col">
+            <div class="hny-content grey-text">
+                <h1>What is Lorem Ipsum?</h1>
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+               
+               <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+
+               <img src="https://www.weddingsutra.com/images/banners/TR01.jpg" class="img-fluid">
+
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+               
+               <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+
+               <img src="https://www.weddingsutra.com/images/banners/TR01.jpg" class="img-fluid">
+
+               <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+
+                <img src="https://www.weddingsutra.com/images/banners/TR01.jpg" class="img-fluid">
+
+
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+               
+               <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
             </div>
-        </div>
-    </section>
+            <h3>Related Articles</h3>
+            <div class="row">
+
+
+               <div class="col-lg-4 col-md-4">
+                  <a href="#">   
+
+                  <div class="honeymoon-thumbnail-img">
+                     <img src="https://www.weddingsutra.com/images/travel/beach_wedding_thumb.jpg" class="img-fluid">
+                     <p>7 Honeymoon</p>
+                     <span>Destination for Game of</span>
+
+
+                  </div>
+              </a>
+               </div>
+               <div class="col-lg-4 col-md-4">
+                 <a href="#"> 
+                  <div class="honeymoon-thumbnail-img">
+                     <img src="https://www.weddingsutra.com/images/travel/got_thumb.jpg" class="img-fluid">
+                     <p>7 Honeymoon</p>
+                     <span>Destination for Game of</span>
+
+
+                  </div>
+              </a>
+               </div>
+               <div class="col-lg-4 col-md-4">
+                     <a href="#"> 
+                  <div class="honeymoon-thumbnail-img">
+                     <img src="https://www.weddingsutra.com/images/travel/divyanka_second_honeymoon_thumb.jpg" class="img-fluid">
+                     <p>7 Honeymoon</p>
+                     <span>Destination for Game of</span>
+
+
+                  </div>
+              </a>
+               </div>
+               <div class="col-lg-4 col-md-4">
+                   <a href="#"> 
+                  <div class="honeymoon-thumbnail-img">
+                     <img src="https://www.weddingsutra.com/images/bestglamping-thumb-nw-350x350.jpg" class="img-fluid">
+                     <p>7 Honeymoon</p>
+                     <span>Destination for Game of</span>
+
+
+                  </div>
+              </a>
+               </div>
+               <div class="col-lg-4 col-md-4">
+                   <a href="#"> 
+                  <div class="honeymoon-thumbnail-img">
+                     <img src="https://www.weddingsutra.com/images/honeymoon-destinations-in-Kerala-pic4-350x350.jpg" class="img-fluid">
+                     <p>7 Honeymoon</p>
+                     <span>Destination for Game of</span>
+
+
+                  </div>
+              </a>
+               </div>
+               <div class="col-lg-4 col-md-4">
+                   <a href="#"> 
+                  <div class="honeymoon-thumbnail-img">
+                     <img src="https://www.weddingsutra.com/images/honeymoon-destinations-in-Kerala-pic4-350x350.jpg" class="img-fluid">
+                     <p>7 Honeymoon</p>
+                     <span>Destination for Game of</span>
+
+
+                  </div>
+              </a>
+               </div>
+            </div>
+
+         </div>
+         <div class="col-xs-12 col-sm-5 vendor-detail-text-col">
+            <div class="inner bg-white-custom">
+               <div class="inner-col">
+                  <p class="title">Lorem Ipsum</p>
+                  <p class="grey-text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it</p>
+               </div>
+               
+               <form class="form-inline-block honeyform">
+                  <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
+                     <option selected>Find your Favorite</option>
+                     <option value="1">One</option>
+                     <option value="2">Two</option>
+                     <option value="3">Three</option>
+                  </select>
+
+
+                  <select class="custom-select my-1 mr-sm-2" id="inlineFormCustomSelectPref">
+                     <option selected>Select Your City </option>
+                     <option value="1">One</option>
+                     <option value="2">Two</option>
+                     <option value="3">Three</option>
+                  </select>
+
+                <button type="submit" class="btn btn-primary my-1">Search</button>
+            </form>
+            </div>
+            
+            
+            
+         </div>
+      </div>
+   </div>
+   </div>
+</section>
 
 @endsection
