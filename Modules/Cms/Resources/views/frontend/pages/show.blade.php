b1dc3245d55c4922d3179bb4a95df4e01dc1981b
vickyorigin <vickywork55@gmail.com>
2022-10-17 16:06:02 +0530

    css fixes

--- Modules/Cms/Resources/views/frontend/pages/show.blade.php
+++ Modules/Cms/Resources/views/frontend/pages/show.blade.php
@@ -3,35 +3,38 @@
 @section('title') {{$$module_name_singular->meta_title}} @endsection
 
 @section('site-meta-tags')
-    <meta name="keyword" content="{{ $$module_name_singular->meta_keywords ? $$module_name_singular->meta_keywords : setting('meta_keyword') }}">
-    <meta name="description" content="{{ $$module_name_singular->meta_description ? $$module_name_singular->meta_description : setting('meta_keyword') }}">
+<meta name="keyword" content="{{ $$module_name_singular->meta_keywords ? $$module_name_singular->meta_keywords : setting('meta_keyword') }}">
+<meta name="description" content="{{ $$module_name_singular->meta_description ? $$module_name_singular->meta_description : setting('meta_keyword') }}">
 @endsection
 
 @section('page-banner')
-    @if($$module_name_singular->featured_image)
-        <section id="page-banner" class="page-banner-height">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="banner-container">
-                        <img src="{{$$module_name_singular->featured_image}}" alt="" class="img-fluid">
-                        <div class="banner-search-col">
-                            <div class="search-header">
-                                <p class="text">{{$$module_name_singular->name}}</p>
-                            </div>
-                        </div>
-                        <div class="container-fluid">
-                            <nav aria-label="breadcrumb">
-                                <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="/">Home</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">{{$$module_name_singular->name}}</li>
-                                </ol>
-                            </nav>
+@if($$module_name_singular->featured_image)
+<!-- page-banner-height -->
+<section id="page-banner" class="">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="banner-container">
+                <div class="vendor-img">
+                    <img class="filter-cls" src="{{$$module_name_singular->featured_image}}" alt="page banner" class="img-fluid">
+                    <div class="banner-search-col">
+                        <div class="search-header">
+                            <p class="text">{{$$module_name_singular->name}}</p>
                         </div>
                     </div>
                 </div>
+                <div class="container-fluid">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="/">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">{{$$module_name_singular->name}}</li>
+                        </ol>
+                    </nav>
+                </div>
             </div>
-        </section>
-    @endif
+        </div>
+    </div>
+</section>
+@endif
 @endsection
 
 @section('content')
@@ -39,7 +42,7 @@
     <div class="container-fluid">
         <div class="row">
             @php
-                $post_details_url = route('frontend.posts.show',[encode_id($$module_name_singular->id), $$module_name_singular->slug]);
+            $post_details_url = route('frontend.posts.show',[encode_id($$module_name_singular->id), $$module_name_singular->slug]);
             @endphp
             <div class="blog-detail-content-col cms-pages-content">
                 <div class="container">
