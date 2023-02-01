8a04cf259effd2d39d48eb15f7be1860c7a754a7
vickyorigin <vickywork55@gmail.com>
2023-02-01 16:30:11 +0530

    add new section in vendor city page

--- resources/views/frontend/vendors/types/inner/content.blade.php
+++ resources/views/frontend/vendors/types/inner/content.blade.php
@@ -16,7 +16,7 @@
 @endif
 
 @if($content->faq_content != '')
-<section id="text-only-section" class="mt-5">
+<section id="text-only-section" class="mt-5 faq-cls">
     <div class="container-fluid">
         <div class="row">
             <div class="col-xs-12 col-sm-12">
