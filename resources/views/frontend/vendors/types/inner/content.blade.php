17f70dc73b8db1e4cbecbe5224f4230410844848
vickyorigin <vickywork55@gmail.com>
2023-02-01 15:54:42 +0530

    add new section in vendor city page

--- resources/views/frontend/vendors/types/inner/content.blade.php
+++ resources/views/frontend/vendors/types/inner/content.blade.php
@@ -1,33 +1,37 @@
 @if($content)
-    @if($content->content != '')
-        <section id="text-only-section" class="grey-section">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-xs-12 col-sm-12">
-                        <div class="text-header">
-                            <div class="text">
-                                {!! $content->content !!}
-                            </div>
-                        </div>
+@if($content->content != '')
+<section id="text-only-section" class="grey-section">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12">
+                <div class="text-header">
+                    <div class="text">
+                        {!! $content->content !!}
                     </div>
                 </div>
             </div>
-        </section>
-    @endif
+        </div>
+    </div>
+</section>
+@endif
 
-    @if($content->faq_content != '')
-        <section id="text-only-section"  class="mt-5" >
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-xs-12 col-sm-12">
-                        <div class="text-header">
-                            <div class="text">
-                                {!! $content->faq_content !!}
-                            </div>
-                        </div>
+@if($content->faq_content != '')
+<section id="text-only-section" class="mt-5">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12">
+                <div class="text-header">
+                    <div class="text">
+                        <?php
+                        $faq = $content->faq_content;
+                        $faq = str_replace(array("<h1>", "<h2>", "<h3>", "<h4>", "<h5>", "<h6>"), "<h4>", $faq);
+                        echo $faq;
+                        ?>
                     </div>
                 </div>
             </div>
-        </section>
-    @endif
+        </div>
+    </div>
+</section>
+@endif
 @endif
