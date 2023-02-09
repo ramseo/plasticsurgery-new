60d6f85ee6155c2b8d234503976f7d4b55e7526f
vickyorigin <vickywork55@gmail.com>
2023-02-09 15:49:41 +0530

    a

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -42,12 +42,20 @@
             <p class="my-4">
                Sign up on {{ env('APP_NAME')}} to reach more couples and book more weddings!
             </p>
-            <a target="_blank" rel="noopener" href="#" class="strt">Start Here <i class="fa fa-arrow-right"></i></a>
+            <a target="_blank" rel="noopener" href="#" class="strt">
+               Start Here
+               <i class="fa fa-arrow-right"></i>
+            </a>
             <h6>Contact us to get best deals</h6>
-            <p class="font-small">{{ env('MAIL_FROM_ADDRESS')}}</p>
             <p class="font-small">
-               <!-- +91 9888898888 -->
-               <?= setting('support_telephone') ?>
+               <a href="mailto:<?= env('MAIL_FROM_ADDRESS') ?>">
+                  <?= env('MAIL_FROM_ADDRESS') ?>
+               </a>
+            </p>
+            <p class="font-small">
+               <a href="tel:<?= setting('support_telephone') ?>">
+                  <?= setting('support_telephone') ?>
+               </a>
             </p>
             <h6>Get Latest Blog Alerts</h6>
             <form id="newsletterForm" action="">
