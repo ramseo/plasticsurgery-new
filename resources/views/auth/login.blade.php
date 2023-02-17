e83bb8b3c1173328b6498c37dbc1f1051ff02802
vickyprivate <vicky.mccreative@gmail.com>
2023-02-17 21:33:56 +0530

    css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -30,7 +30,7 @@
         @if(env('FACEBOOK_ACTIVE') || env('GITHUB_ACTIVE') || env('GOOGLE_ACTIVE'))
             <div class="text-center">
                 @if(env('FACEBOOK_ACTIVE'))
-                <a href="{{route('facebook.login')}}" class="btn btn-block btn-outline-info">
+                <a href="{{route('social.login', 'facebook')}}" class="btn btn-block btn-outline-info">
                     <span class="btn-inner--icon"> <i class="fab fa-facebook"></i> </span>
                     <span class="btn-inner--text">Log in with Facebook</span>
                 </a>
