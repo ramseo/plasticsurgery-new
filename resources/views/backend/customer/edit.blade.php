8ae693e733fbb64d482dc81294bc01b002692d7c
vickyorigin <vickywork55@gmail.com>
2023-05-30 14:34:02 +0530

    add

--- resources/views/backend/customer/edit.blade.php
+++ resources/views/backend/customer/edit.blade.php
@@ -151,11 +151,11 @@
                 {{-- </div>--}}
 
                 <div class="row">
-                    <div class="col-sm-4">
+                    <!-- <div class="col-sm-4">
                         <div class="form-group">
                             {{ html()->submit($text = icon('fas fa-save')." Save")->class('btn btn-success') }}
                         </div>
-                    </div>
+                    </div> -->
 
                     <div class="col-sm-8">
                         <div class="float-right">
