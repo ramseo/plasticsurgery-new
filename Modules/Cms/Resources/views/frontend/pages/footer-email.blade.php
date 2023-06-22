82a1ce1cd3165f432207a8afbe85a9434a4ca00c
vickyorigin <vickywork55@gmail.com>
2023-06-22 13:35:35 +0530

    ad

---
+++ Modules/Cms/Resources/views/frontend/pages/footer-email.blade.php
@@ -0,0 +1,19 @@
+<div class="container-fluid footer-email-sec">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="item-middle">
+                    <div class="ico">
+                        <i class="fa fa-paper-plane-o" aria-hidden="true"></i>
+                        <h6>For any query related to treatment email us</h6>
+                        <h6>
+                            <a href="mailto:<?= Setting('email') ?>">
+                                <?= Setting('email') ?>
+                            </a>
+                        </h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/frontend/reconstructive.blade.php
+++ resources/views/frontend/reconstructive.blade.php
@@ -176,25 +176,7 @@
 </section>
 
 <div class="spacer">
-    <div class="container-fluid">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="item-middle">
-                        <div class="ico">
-                            <i class="fa fa-paper-plane-o" aria-hidden="true"></i>
-                            <h6>For any query related to treatment email us</h6>
-                            <h6>
-                                <a href="mailto:<?= Setting('email') ?>">
-                                    <?= Setting('email') ?>
-                                </a>
-                            </h6>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('cms::frontend.pages.footer-email')
 </div>
 
 @endsection
