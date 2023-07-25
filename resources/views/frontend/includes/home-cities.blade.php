f30c5ab0e870bfcc0d4dd4af818ef13b7e5d4996
singhsimarjot <simarjots9@gmail.com>
2023-07-25 11:18:20 +0530

    new add file

--- resources/views/frontend/includes/home-cities.blade.php
+++ resources/views/frontend/includes/home-cities.blade.php
@@ -1,41 +1,11 @@
-<div class="container-fluid location">
-    <div class="container">
-        <h2 class="title text-center fnd-surgeon mb-2">
-            Find a Surgeon by City
-</h2>
-<hr>
-        <div class="row">
-            <?php
-            $citiesArr = citiesArr();
-            if ($citiesArr) {
-                foreach ($citiesArr as $city) {
-            ?>
-                    <div class="col-lg-3 col-md-4">
-                        <div class="cities">
-                            <a href="<?= url(strtolower($city)) ?>">
-                                <h3>
-                                    <i class="fa fa-map-marker blink" aria-hidden="true"></i>
-                                    &nbsp;
-                                    <?= $city ?>
-                                </h3>
-                            </a>
-                        </div>
-                    </div>
-            <?php
-                }
-            }
-            ?>
-        </div>
-    </div>
-</div>
-
 <div class="container-fluid new-doctor">
     <div class="container">
         <div class="row">
             <div class="col-lg-6 col-md-6">
                 <div class="sr-heading">
                     <p>Find the Top Surgeons in India by Procedure</p>
-                    <select class="form-control cutome_sele_bg" onchange="(this.options[this.selectedIndex].value? window.open(this.options[this.selectedIndex].value,'_blank'):'')">
+                    <select class="form-control cutome_sele_bg"
+                        onchange="(this.options[this.selectedIndex].value? window.open(this.options[this.selectedIndex].value,'_blank'):'')">
                         <option selected="selected" value="">Select a Procedure</option>
                         <option value="<?= url('hair-transplant') ?>">Hair Transplant</option>
                         <option value="<?= url('gynecomastia') ?>">Gynecomastia</option>
@@ -56,7 +26,8 @@
             <div class="col-lg-6 col-md-6">
                 <div class="sr-heading">
                     <p>Find the Cost of a Procedure</p>
-                    <select class="form-control fm-cont cutome_sele_bg" onchange="(this.options[this.selectedIndex].value? window.open(this.options[this.selectedIndex].value,'_blank'):'')">
+                    <select class="form-control fm-cont cutome_sele_bg"
+                        onchange="(this.options[this.selectedIndex].value? window.open(this.options[this.selectedIndex].value,'_blank'):'')">
                         <option selected="selected" value="">Select a Procedure</option>
                         <option value="<?= url('hair-transplant') ?>">Hair Transplant</option>
                         <option value="<?= url('gynecomastia') ?>">Gynecomastia</option>
@@ -74,6 +45,37 @@
                     </select>
                 </div>
             </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid location">
+    <div class="container">
+        <h2 class="title text-center fnd-surgeon mb-2">
+            Find a Surgeon by City
+        </h2>
+        <hr>
+        <div class="row">
+            <?php
+            $citiesArr = citiesArr();
+            if ($citiesArr) {
+                foreach ($citiesArr as $city) {
+            ?>
+            <div class="col-lg-3 col-md-4">
+                <div class="cities">
+                    <a href="<?= url(strtolower($city)) ?>">
+                        <h3>
+                            <i class="fa fa-map-marker blink" aria-hidden="true"></i>
+                            &nbsp;
+                            <?= $city ?>
+                        </h3>
+                    </a>
+                </div>
+            </div>
+            <?php
+                }
+            }
+            ?>
         </div>
     </div>
 </div>
--- resources/views/frontend/includes/make-us-different.blade.php
+++ resources/views/frontend/includes/make-us-different.blade.php
@@ -14,28 +14,31 @@
                     <img src="<?= asset("img/plastic-img/doctor-qui.png") ?>" alt="plasticsurgery">
                 </span>
                 <h2>Qualified Plastic Surgeons</h2>
-                <p>We help you to select the most qualified and experienced Board Certified Plastic Surgeons for your treatment.</p>
+                <p>We help you to select the most qualified and experienced Board Certified Plastic Surgeons for your
+                    treatment.</p>
             </div>
             <div class="col-md-4 whatmakes">
                 <span class="whatmakes2">
                     <img src="<?= asset("img/plastic-img/plastic-cosmetic-surgery-result.png") ?>" alt="plasticsurgery">
                 </span>
                 <h2>Effective Results</h2>
-                <p>You get the most effective and realistic results from our plastic and cosmetic surgery procedures.</p>
+                <p>You get the most effective and realistic results from our plastic and cosmetic surgery procedures.
+                </p>
             </div>
             <div class="col-md-4 whatmakes">
                 <span class="whatmakes2">
                     <img src="<?= asset("img/plastic-img/van.png") ?>" alt="plasticsurgery">
                 </span>
                 <h2>Safe Treatments</h2>
-                <p>Our Plastic Surgeons strictly abide by all the protocols to ensure safe and successful procedures.</p>
+                <p>Our Plastic Surgeons strictly abide by all the protocols to ensure safe and successful procedures.
+                </p>
             </div>
         </div>
     </div>
 </section>
 
-<section class="home-section make-us-img">
+<!-- <section class="home-section make-us-img">
     <div class="container text-center">
         <img src="<?= asset("img/plastic-img/ps-ba.jpg") ?>" alt="plasticsurgery" class="img-responsive">
     </div>
-</section>
+</section> -->
